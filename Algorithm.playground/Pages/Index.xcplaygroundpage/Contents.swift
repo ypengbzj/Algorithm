4771ba8d8d756c9b14afb4d51a01ba657b8289cc
Craigyu <peng_ios@163.com>
2025-03-13 10:55:15 +0800

    feat： Add Binary Search

--- Algorithm.playground/Pages/Index.xcplaygroundpage/Contents.swift
+++ Algorithm.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -5,9 +5,13 @@
  
  [代码讲解](https://github.com/kodecocodes/swift-algorithm-club)
  
+ ## 可视化工具
+ * [VisuAlgo](https://visualgo.net/) - 可视化算法和数据结构
+ 
  ## 目录
  
  * [Stack](Stack)
  * [Queue](Queue)
- * [InsertionSort](InsertionSort)
+ * [插入排序(InsertionSort)](InsertionSort)
+ * [二分查找(BinarySearch)](BinarySearch)
  */
